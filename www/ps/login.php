42797536a2c7803ca634da296cbae33fb056c461
Fabián Gonzalo Artur de la Villarmois <gonzartur@gmail.com>
2013-11-25 12:37:22 -0200

    Añadidos archivos login-soft.css, jquery.validate.min.js, login-soft.js y login_soft.php necesarios para la correcta visualizacion del ultimo archivo. Incluidas clases class.uFlex.php y functions.php. Implementados procesos en ps/ login.php y logout.php. Incluido archivo de configuraciones config.php. Modificado index.html para direccionar al proceso logout.

---
+++ www/ps/login.php
@@ -0,0 +1,26 @@
+<?php
+
+
+//Proccess Login
+if(count($_POST)){
+	$username = isset($_POST['username']) ? $_POST['username'] : false;
+	$password = isset($_POST['password']) ? $_POST['password'] : false;
+	$auto = isset($_POST['auto']) ? $_POST['auto'] : false;
+
+	$user->login($username,$password,$auto);
+
+	$errMsg = '';
+
+	if($user->has_error()){
+		$errMsg = $user->error();
+		$errMsg = $errMsg[0];
+		echo json_encode(array(
+		'error'    => $user->error(),
+		'form'     => $user->form_error(),
+		))
+	}	
+	
+	if($user->signed) redirect("../index.html");
+	else redirect("../login_soft.php");
+}
+?>
